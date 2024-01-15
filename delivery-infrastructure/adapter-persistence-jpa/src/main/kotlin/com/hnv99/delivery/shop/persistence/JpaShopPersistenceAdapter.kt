848785b5dc21d5654ed2f18e4545d790029fa222
Hung Nguyen Van <vanhung4499@gmail.com>
2024-01-16 00:49:01 +0700

    refactor jpa persistence module

--- delivery-infrastructure/adapter-persistence-jpa/src/main/kotlin/com/hnv99/delivery/shop/persistence/JpaShopPersistenceAdapter.kt
+++ delivery-infrastructure/adapter-persistence-jpa/src/main/kotlin/com/hnv99/delivery/shop/persistence/JpaShopPersistenceAdapter.kt
@@ -3,9 +3,11 @@ package com.hnv99.delivery.shop.persistence
 import com.hnv99.delivery.common.exception.EntityNotFoundException
 import com.hnv99.delivery.reservation.persistence.ReservationRepository
 import com.hnv99.delivery.shop.application.port.output.*
+import com.hnv99.delivery.shop.domain.product.Product
 import com.hnv99.delivery.shop.domain.shop.Shop
 import org.springframework.stereotype.Repository
-import java.util.*
+import java.time.LocalTime
+import java.util.UUID
 
 private const val NOT_FOUND_SHOP_MESSAGE = "Shop information not found."
 @Repository
@@ -22,19 +24,25 @@ class JpaShopPersistenceAdapter(
 
     override fun loadAllShop(): List<Shop> = shopRepository.findAll()
 
-    override fun loadShopById(shopId: UUID) = getShopOrThrow(shopRepository::findOneWithProductsById, shopId)
+    override fun loadShopById(shopId: UUID): Shop {
+        return shopRepository.findById(shopId)
+            .orElseThrow { throw EntityNotFoundException(NOT_FOUND_SHOP_MESSAGE) }
+    }
 
-    override fun loadAllProductByShopId(shopId: UUID) =
-        getShopOrThrow(shopRepository::findOneWithProductsById, shopId).products
+    override fun loadAllProductByShopId(shopId: UUID): List<Product> {
+        return shopRepository.findWithProductsById(shopId)
+            .orElseThrow { throw EntityNotFoundException(NOT_FOUND_SHOP_MESSAGE) }
+    .products
+    }
 
-    override fun loadAllReservableTimeByShopId(shopId: UUID) =
-        getShopOrThrow(shopRepository::findOneWithReservableTimesById, shopId).reservableTimes
+    override fun loadAllReservableTimeByShopId(shopId: UUID): List<LocalTime> {
+        return shopRepository.findWithReservableTimesById(shopId)
+            .orElseThrow { throw EntityNotFoundException(NOT_FOUND_SHOP_MESSAGE) }
+            .reservableTimes
+    }
 
     override fun hasReservations(shopId: UUID): Boolean {
         return reservationRepository.existsReservationByShopId(shopId)
     }
 
-    private fun getShopOrThrow(queryMethod: Function<UUID, Optional<Shop>>, shopId: UUID): Shop {
-        return (queryMethod.apply(shopId).orElseThrow { throw EntityNotFoundException(NOT_FOUND_SHOP_MESSAGE) })
-    }
 }
--- delivery-infrastructure/adapter-persistence-jpa/src/main/kotlin/com/hnv99/delivery/shop/persistence/ShopRepository.kt
+++ delivery-infrastructure/adapter-persistence-jpa/src/main/kotlin/com/hnv99/delivery/shop/persistence/ShopRepository.kt
@@ -1,15 +1,14 @@
 package com.hnv99.delivery.shop.persistence
 
 import com.hnv99.delivery.shop.domain.shop.Shop
-import net.bytebuddy.dynamic.DynamicType.Builder.RecordComponentDefinition.Optional
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
+import java.util.*
 
-interface ShopRepository : JpaRepository<Shop, Long> {
+interface ShopRepository : JpaRepository<Shop, UUID> {
     @EntityGraph(attributePaths = ["products"])
-    fun findOneWithProductsById(shopId: UUID): Optional<Shop>
+    fun findWithProductsById(shopId: UUID): Optional<Shop>
 
     @EntityGraph(attributePaths = ["reservableTimes"])
-    fun findOneWithReservableTimesById(shopId: UUID): Optional<Shop>
+    fun findWithReservableTimesById(shopId: UUID): Optional<Shop>
 }
